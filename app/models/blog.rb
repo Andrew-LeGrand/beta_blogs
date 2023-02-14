303f3fb5886ef336f2389bca4ce1fc1e42180410
Andrew-LeGrand <111323440+Andrew-LeGrand@users.noreply.github.com>
2023-02-13 18:56:45 -0600

    Initial Commit
    Generated project, added pages controller, set up navbar and routing

---
+++ app/models/blog.rb
@@ -0,0 +1,20 @@
+class Blog < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 2, maximum: 200}
+    validates :content, presence: true, length: {minimum: 2, maximum: 200}
+
+    validate :image_path_exists
+
+    def image_path_exists
+        require 'open-uri'
+
+        begin
+            # is the url valid?
+            # if thrown an exception, then rescue will execute
+            path = URI.open(image_path)
+            # does the response have a content type image?
+            errors.add(:image_path, "does not contain image content") and return unless path.content_type.starts_with?("image")
+        rescue
+            errors.add(image_path, "invalid url")
+        end        
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root to: "pages#home"
+  get '/about-us', to: "pages#about_us"
+end
